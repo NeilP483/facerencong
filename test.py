4b806d5674797ce57eedef48c17c94e36e685d29
NeilP483 <neilp235094@gmail.com>
2023-08-21 15:53:13 -0400

    Added Face tracking

--- test.py
+++ test.py
@@ -8,10 +8,9 @@ while(cam.isOpened()):
     if ret:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces  = face_cascade.detectMultiScale(gray, 1.3, 5)
-        final = frame
         for (x, y, w, h) in faces:
-            final = frame[y:y + h, x:x + w]
-        cv2.imshow("Video", final)
+            cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
+        cv2.imshow("Video", frame)
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
 cam.release()
